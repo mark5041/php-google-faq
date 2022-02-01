98f1f95d539b6483e11082b2f971d6a55368289b
mark5041 <mark.beloso5041@gmail.com>
2022-02-01 15:57:37 +0100

    normal question

--- index.php
+++ index.php
@@ -85,9 +85,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+
+    <div class="container">
+        <?php
+            for($i = 0; $i < 5; $i++)
+            {
+                if($FAQ['answare'][$i]['type'] == 'normal')
+                {
+                    echo    '<div class="question">
+                                <h1>'.$FAQ['question'][$i].'</h1>
+                                <div class="answare">
+                                    <p>'.$FAQ['answare'][$i]['text'].'</p>
+                                </div>
+                            </div>';
+                }
+            }
+        ?>
+    </div>
     
 </body>
 </html>
